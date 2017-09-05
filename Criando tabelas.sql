3fe7adb74c8adbc349df7dd0c802d872aeb50bb7
Lincoln Ferreira Campos <link7-7@hotmail.com>
2017-09-05 19:54:48 -0300

    -Banco de dados definido! -classe de conaxao pronta! -sistema atualizado- -bootstrap implementado.

---
+++ Criando tabelas.sql
@@ -0,0 +1,30 @@
+﻿Create table Funcionario(
+
+IdFuncionario			integer				identity(1,1),
+NomeFuncionario			nvarchar(50)		not null,
+Funcao					nvarchar(25)		not null,
+primary key(IdFuncionario),
+check( Funcao in('Gerente', 'Analista','Programador','Designer','DBA'))
+
+)
+go
+
+Create table Projeto (
+
+IdProjeto				integer			identity(1,1),
+NomeProjeto				nvarchar(50)		not null,
+DateInicio				date				not null,
+DateFim					date				not null,
+primary key(IdProjeto)
+)
+go
+
+Create table ProjetoFuncionario(
+
+IdProjeto			integer			not null,
+IdFuncionario		integer			not null,
+primary key(IdProjeto, IdFuncionario),
+foreign key(IdProjeto) references Projeto(IdProjeto),
+foreign key(IdFuncionario) references Funcionario(IdFuncionario)
+)
+go
---
+++ Projeto.DAL/Conexao.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Entidades;
+using System.Data.SqlClient;
+
+namespace Projeto.DAL
+{
+    public class Conexao
+    {
+        protected SqlConnection con;
+        protected SqlCommand cmd;
+        protected SqlDataReader dr;
+        protected SqlTransaction tr;
+
+
+        protected void OpenConnect()
+        {
+            con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\COTI informatica\GitHub 18\Projeto.WEB\App_Data\Banco.mdf;Integrated Security=True");
+            con.Open();
+        }
+
+        protected void CloseConnect()
+        {
+            con.Close();
+        }
+    }
+}
