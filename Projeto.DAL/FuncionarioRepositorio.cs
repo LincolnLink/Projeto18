b0e1daa3dd2f429615e84b31118d33e3a3699533
Lincoln Ferreira Campos <link7-7@hotmail.com>
2017-09-06 06:01:30 -0300

    commit do dia(quarta)

--- Projeto.DAL/FuncionarioRepositorio.cs
+++ Projeto.DAL/FuncionarioRepositorio.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Data.SqlClient;
-using Projeto.Entidades.Tipos;
-using Projeto.Entidades;
+using System.Data.SqlClient; //SQL
+using Projeto.Entidades.Tipos; //Enum
+using Projeto.Entidades; //entidades
 
 namespace Projeto.DAL
 {
     public class FuncionarioRepositorio : Conexao
     {
+
+        #region Imprimir Tabela
         public List<Funcionario> FindAll()
         {
             OpenConnection();
@@ -27,7 +29,9 @@ namespace Projeto.DAL
                 Funcionario f = new Funcionario();
                 f.IdFuncionario = Convert.ToInt32(dr["IdFuncionario"]);
                 f.NomeFuncionario = Convert.ToString(dr["NomeFuncionario"]);
-
+                /// <summary>
+                /// Converte o dado em string, para depois converter em um Enum!
+                /// </summary>
                 string fucao = Convert.ToString(dr["Funcao"]);
                 f.Funcao = (Funcao) Enum.Parse(typeof(Funcao), fucao);
 
@@ -37,5 +41,11 @@ namespace Projeto.DAL
             CloseConnection();
             return lista;
         }
+        #endregion
+
+
+        #region Inserindo
+
+        #endregion
     }
 }
